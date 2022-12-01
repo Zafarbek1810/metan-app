709ee38dfc02d523671b4565467f3da845f5890e
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2022-12-01 14:46:01 +0500

    bootsrap js add

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import $ from 'jquery';
 import Popper from 'popper.js';
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
 import {useEffect, useState} from "react";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({Component, pageProps}) {
   const [showChild, setShowChild] = useState(false);
@@ -20,9 +22,26 @@ function MyApp({Component, pageProps}) {
     return <></>;
   } else {
     return (
-      <UserContextProvider>
-        <Component {...pageProps} />
-      </UserContextProvider>
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+            crossOrigin="anonymous"
+          />
+        </Head>
+
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+          crossOrigin="anonymous"
+        />
+        <UserContextProvider>
+          <Component {...pageProps} />
+        </UserContextProvider>
+      </>
     )
   }
 }
