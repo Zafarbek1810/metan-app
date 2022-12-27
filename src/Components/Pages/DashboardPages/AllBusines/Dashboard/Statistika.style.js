5b3a95cc4d6cab40e51715cb107a0b23d0c321c4
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2022-12-28 01:53:56 +0500

    style fix

--- src/Components/Pages/DashboardPages/AllBusines/Dashboard/Statistika.style.js
+++ src/Components/Pages/DashboardPages/AllBusines/Dashboard/Statistika.style.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 const StatistikaWrapper=styled.div`
-
+  .MuiPaper-root {
+    width: 100% !important;
+  }
 `
 
 export{StatistikaWrapper}
--- src/Components/Pages/DashboardPages/AllBusines/Dashboard/index.jsx
+++ src/Components/Pages/DashboardPages/AllBusines/Dashboard/index.jsx
@@ -25,152 +25,156 @@ import {Wrapper} from "../../Home/style";
 import React from "react";
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
 import {Card, Col, Progress, Row, Statistic} from 'antd';
+import {StatistikaWrapper} from "./Statistika.style";
 
 const Dashboard = () => {
     return (
-        <Wrapper>
-            <ApexChartWrapper>
-                <div className="site-statistic-demo-card mb-4">
-                    <Row gutter={16}>
-                        <Col span={8}>
-                            <Card>
-                                <Statistic
-                                    style={{marginBottom:20}}
-                                    title="Kirimlar"
-                                    value={"129 000 000"}
-                                    precision={2}
-                                    valueStyle={{
-                                        color: '#3f8600',
-                                    }}
-                                    prefix={<ArrowUpOutlined />}
-                                    suffix="UZS"
-                                />
-                                <Progress
-                                    percent={69}
-                                    strokeColor={{
-                                        '0%': '#108ee9',
-                                        '100%': '#87d068',
-                                    }}
-                                />
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card>
-                                <Statistic
-                                    style={{marginBottom:20}}
-                                    title="Chiqimlar"
-                                    value={"11 500 000"}
-                                    precision={2}
-                                    valueStyle={{
-                                        color: '#cf1322',
-                                    }}
-                                    prefix={<ArrowDownOutlined />}
-                                    suffix="UZS"
-                                />
-                                <Progress
-                                    percent={36}
-                                    strokeColor={{
-                                        '0%': '#899FD4',
-                                        '100%': '#899FD4',
-                                    }}
-                                />
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card>
-                                <Statistic
-                                    style={{marginBottom:20}}
-                                    title="Hisobdagi pullar"
-                                    value={"24 000 000"}
-                                    precision={2}
-                                    valueStyle={{
-                                        color: '#3f8600',
-                                    }}
-                                    prefix={<ArrowUpOutlined />}
-                                    suffix="UZS"
-                                />
-                                <Progress
-                                    percent={69}
-                                    strokeColor={{
-                                        '0%': '#1de9b6',
-                                        '100%': '#1dc4e9',
-                                    }}
-                                />
-                            </Card>
-                        </Col>
-                    </Row>
-                </div>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={4}>
-                        <Trophy />
-                    </Grid>
-                    <Grid item xs={12} md={8}>
-                        <StatisticsCard />
-                    </Grid>
-                    <Grid item xs={12} md={6} lg={4}>
-                        <WeeklyOverview />
-                    </Grid>
-                    <Grid item xs={12} md={6} lg={4}>
-                        <TotalEarning />
-                    </Grid>
-                    <Grid item xs={12} md={6} lg={4}>
-                        <Grid container spacing={3}>
-                            <Grid item xs={6}>
-                                <CardStatisticsVerticalComponent
-                                    stats='$25.6k'
-                                    icon={<Poll />}
-                                    color='success'
-                                    trendNumber='+42%'
-                                    title='Total Profit'
-                                    subtitle='Weekly Profit'
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <CardStatisticsVerticalComponent
-                                    stats='$78'
-                                    title='Refunds'
-                                    trend='negative'
-                                    color='secondary'
-                                    trendNumber='-15%'
-                                    subtitle='Past Month'
-                                    icon={<CurrencyUsd />}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <CardStatisticsVerticalComponent
-                                    stats='862'
-                                    trend='negative'
-                                    trendNumber='-18%'
-                                    title='New Project'
-                                    subtitle='Yearly Project'
-                                    icon={<BriefcaseVariantOutline />}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <CardStatisticsVerticalComponent
-                                    stats='15'
-                                    color='warning'
-                                    trend='negative'
-                                    trendNumber='-18%'
-                                    subtitle='Last Week'
-                                    title='Sales Queries'
-                                    icon={<HelpCircleOutline />}
-                                />
+        <StatistikaWrapper>
+            <Wrapper>
+                <ApexChartWrapper>
+                    <div className="site-statistic-demo-card mb-4">
+                        <Row gutter={16}>
+                            <Col span={8}>
+                                <Card>
+                                    <Statistic
+                                        style={{marginBottom:20}}
+                                        title="Kirimlar"
+                                        value={"129 000 000"}
+                                        precision={2}
+                                        valueStyle={{
+                                            color: '#3f8600',
+                                        }}
+                                        prefix={<ArrowUpOutlined />}
+                                        suffix="UZS"
+                                    />
+                                    <Progress
+                                        percent={69}
+                                        strokeColor={{
+                                            '0%': '#108ee9',
+                                            '100%': '#87d068',
+                                        }}
+                                    />
+                                </Card>
+                            </Col>
+                            <Col span={8}>
+                                <Card>
+                                    <Statistic
+                                        style={{marginBottom:20}}
+                                        title="Chiqimlar"
+                                        value={"11 500 000"}
+                                        precision={2}
+                                        valueStyle={{
+                                            color: '#cf1322',
+                                        }}
+                                        prefix={<ArrowDownOutlined />}
+                                        suffix="UZS"
+                                    />
+                                    <Progress
+                                        percent={36}
+                                        strokeColor={{
+                                            '0%': '#899FD4',
+                                            '100%': '#899FD4',
+                                        }}
+                                    />
+                                </Card>
+                            </Col>
+                            <Col span={8}>
+                                <Card>
+                                    <Statistic
+                                        style={{marginBottom:20}}
+                                        title="Hisobdagi pullar"
+                                        value={"24 000 000"}
+                                        precision={2}
+                                        valueStyle={{
+                                            color: '#3f8600',
+                                        }}
+                                        prefix={<ArrowUpOutlined />}
+                                        suffix="UZS"
+                                    />
+                                    <Progress
+                                        percent={69}
+                                        strokeColor={{
+                                            '0%': '#1de9b6',
+                                            '100%': '#1dc4e9',
+                                        }}
+                                    />
+                                </Card>
+                            </Col>
+                        </Row>
+                    </div>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} md={4}>
+                            <Trophy />
+                        </Grid>
+                        <Grid item xs={12} md={8}>
+                            <StatisticsCard />
+                        </Grid>
+                        <Grid item xs={12} md={6} lg={4}>
+                            <WeeklyOverview />
+                        </Grid>
+                        <Grid item xs={12} md={6} lg={4}>
+                            <TotalEarning />
+                        </Grid>
+                        <Grid item xs={12} md={6} lg={4}>
+                            <Grid container spacing={3}>
+                                <Grid item xs={6}>
+                                    <CardStatisticsVerticalComponent
+                                        stats='$25.6k'
+                                        icon={<Poll />}
+                                        color='success'
+                                        trendNumber='+42%'
+                                        title='Total Profit'
+                                        subtitle='Weekly Profit'
+                                    />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <CardStatisticsVerticalComponent
+                                        stats='$78'
+                                        title='Refunds'
+                                        trend='negative'
+                                        color='secondary'
+                                        trendNumber='-15%'
+                                        subtitle='Past Month'
+                                        icon={<CurrencyUsd />}
+                                    />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <CardStatisticsVerticalComponent
+                                        stats='862'
+                                        trend='negative'
+                                        trendNumber='-18%'
+                                        title='New Project'
+                                        subtitle='Yearly Project'
+                                        icon={<BriefcaseVariantOutline />}
+                                    />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <CardStatisticsVerticalComponent
+                                        stats='15'
+                                        color='warning'
+                                        trend='negative'
+                                        trendNumber='-18%'
+                                        subtitle='Last Week'
+                                        title='Sales Queries'
+                                        icon={<HelpCircleOutline />}
+                                    />
+                                </Grid>
                             </Grid>
                         </Grid>
+                        <Grid item xs={12} md={6} lg={4}>
+                            <SalesByCountries />
+                        </Grid>
+                        <Grid item xs={12} md={12} lg={8}>
+                            <DepositWithdraw />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Table />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} md={6} lg={4}>
-                        <SalesByCountries />
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={8}>
-                        <DepositWithdraw />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Table />
-                    </Grid>
-                </Grid>
-            </ApexChartWrapper>
-        </Wrapper>
+                </ApexChartWrapper>
+            </Wrapper>
+        </StatistikaWrapper>
+
     )
 }
 
