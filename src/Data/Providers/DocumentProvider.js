8f521c4e7d3dc2b1604440eb1c4f59db90e6ca9a
Javokhir Shomuratov <javohirshomuradov@gmail.com>
2023-01-12 10:12:56 +0500

    fixed document upload

--- src/Data/Providers/DocumentProvider.js
+++ src/Data/Providers/DocumentProvider.js
@@ -2,7 +2,26 @@ import client from "../../HHTP/client";
 
 export default class DocumentProvider {
     static async createDocument(body) {
-        return await client.post("/document", body);
+
+        // axios({
+        //     method: "post",
+        //     url: "myurl",
+        //     data: bodyFormData,
+        //     headers: { "Content-Type": "multipart/form-data" },
+        //   })
+        //     .then(function (response) {
+        //       //handle success
+        //       console.log(response);
+        //     })
+        //     .catch(function (response) {
+        //       //handle error
+        //       console.log(response);
+        //     });
+        console.log(client.headers);
+        console.log(body);
+        return await client.post("/document", body, {
+            headers: { 'Content-Type': 'multipart/form-data' },
+        });
     }
     static async getAllDocument() {
         return await client.get(`/document`);
